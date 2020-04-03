baeec9c7e32b05a78d22eaee7613448cf21c95fc
Nikita Vinokurov <vinokurov.csc@yandex.ru>
2020-04-03 15:42:23 +0300

    CSV information container created

--- src/CSVTableInformationContainer.java
+++ src/CSVTableInformationContainer.java
@@ -1,2 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class CSVTableInformationContainer {
+    private String[] columnNames;
+    private String[][] table;
+    private int rows;
+    private int cols;
+
+    public CSVTableInformationContainer(BufferedReader reader) throws IOException {
+        rows = CSVInformationParser.parseRows(reader);
+        cols = CSVInformationParser.parseCols(reader);
+        columnNames = CSVInformationParser.parseColumnNames(reader);
+        table = CSVInformationParser.parseTable(reader, cols, rows, columnNames);
+    }
+
+    public String[] getColumnNames() {
+        return columnNames;
+    }
+
+    public String[][] getTable() {
+        return table;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
 }
